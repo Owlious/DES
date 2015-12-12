39f014047e842a92cecde130b0064919b43d5ea8
Owlious <544922835@qq.com>
2015-12-12 14:39:16 +0800

    Make it available

---
+++ DES_En/DES.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DES_En
+{
+    public class DES
+    {
+        public string InputStr { get; set; }
+        public string OutputStr { get; set; }
+        public string Key { get; set; }
+        public string IV { get; set; }
+
+        public bool Encrypt()
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            IV = Key;
+            byte[] KeyBt = Encoding.UTF8.GetBytes(Key);
+            byte[] IVBt = Encoding.UTF8.GetBytes(IV);
+            
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] indata = Encoding.Default.GetBytes(InputStr);
+                try
+                {
+                    CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(KeyBt, IVBt), CryptoStreamMode.Write);
+                    cs.Write(indata, 0, indata.Length);
+                    cs.FlushFinalBlock();
+                    OutputStr = Convert.ToBase64String(ms.ToArray());
+                    return true;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+        public bool Decrypt()
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            IV = Key;
+            byte[] KeyBt = Encoding.UTF8.GetBytes(Key);
+            byte[] IVBt = Encoding.UTF8.GetBytes(IV);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] indata = Convert.FromBase64String(InputStr);
+                try
+                {
+                    CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(KeyBt, IVBt), CryptoStreamMode.Write);
+                    cs.Write(indata, 0, indata.Length);
+                    cs.FlushFinalBlock();
+                    OutputStr = Encoding.Default.GetString(ms.ToArray());
+                    return true;
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ DES_En/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DES_En;
+
+namespace DES_En
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DES en = new DES();
+            en.InputStr = textBox_M.Text;
+            en.Key = textBox_Key.Text;
+            en.Encrypt();
+            textBox_C.Text = en.OutputStr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DES de = new DES();
+            de.InputStr = textBox_C.Text;
+            de.Key = textBox_Key.Text;
+            de.Decrypt();
+            textBox_CM.Text = de.OutputStr;
+        }
+    }
+}
